20b87a94e0b1ed60059f6088103fb9a18fe6e3d8
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-03 09:06:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function produceDrivingRange(blockRange) {
+  return (start, destination) => {
+    const distance = parseInt(destination) - parseInt(start);
+    if (distance < blockRange) {
+      return `within range by ${distance}`
+    } else {
+      return `${distance - blockRange} blocks out of range`;
+    }
+  };
+}
+
+function produceTipCalculator(percent) {
+  
+}
